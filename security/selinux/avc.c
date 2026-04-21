68f5d2236db87a7afc62b5d3d26d37dfbbfa5fef
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-04-21 19:38:10 -0400

    fs: Implement scope-minimized manual KernelSU calls/hooks v2.1 and abolish the dependence upon KProbes
    Co-Authored-By: JackAltman <82563979+jacka1ltman@users.noreply.github.com>
    Co-Authored-By: simonpunk <simonpunk2016@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -780,6 +780,10 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 	}
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE) && !defined(CONFIG_KSU_KPROBES_HOOK)
+extern void ksu_slow_avc_audit(u32 *tsid);
+#endif
+
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(struct selinux_state *state,
 			    u32 ssid, u32 tsid, u16 tclass,
@@ -790,6 +794,9 @@ noinline int slow_avc_audit(struct selinux_state *state,
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	ksu_slow_avc_audit(&tsid);
+#endif
 	if (!a) {
 		a = &stack_data;
 		a->type = LSM_AUDIT_DATA_NONE;
